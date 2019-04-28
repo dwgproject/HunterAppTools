9934b87202ae6e26985ea55b6addb05623949e39
patrykapriasz <patrykapriasz@gmail.com>
2019-04-28 11:25:01 +0200

    CsvWriter poprawki

--- Src/Tools/CsvWriter.cs
+++ Src/Tools/CsvWriter.cs
@@ -7,35 +7,49 @@ using System.IO;
 using System.Reflection;
 using System.Text;
 using Gravityzero.Console.Utility.Model;
+using log4net;
+using Gravityzero.Console.Utility.Logger;
 
 namespace Gravityzero.Console.Utility.Tools
 {
     public class CsvWriter<TData>
     {
-        public void WriteFile(string path, List<TData> data)
+
+        private readonly ILog log = LogManager.GetLogger(typeof(CsvWriter<TData>));
+        public CsvWriter()
+        {
+            LoggerConfiguration.LoadConfiguration();
+        }
+        public bool WriteFile(string path, List<TData> data)
         {
             var tmp = new StringBuilder();
-
-            foreach(var item in data){
-                var newLine = new StringBuilder();
-                foreach(var oneObject in item.GetType().GetProperties()){
-                    var propertyValue = oneObject.GetValue(item, null);
-                    if(oneObject.PropertyType.IsClass && !oneObject.PropertyType.FullName.StartsWith("System.")){
-                        // var x = oneObject.PropertyType.GetProperty("Identifier").PropertyType;
-                        propertyValue = (Guid?)oneObject.PropertyType.GetProperty("Identifier" ).GetValue(oneObject.GetValue(item,null),null);
+            try{
+                foreach(var item in data){
+                    var newLine = new StringBuilder();
+                    foreach(var oneObject in item.GetType().GetProperties()){
+                        var propertyValue = oneObject.GetValue(item, null);
+                        if(oneObject.PropertyType.IsClass && !oneObject.PropertyType.FullName.StartsWith("System.")){
+                            // var x = oneObject.PropertyType.GetProperty("Identifier").PropertyType;
+                            propertyValue = (Guid?)oneObject.PropertyType.GetProperty("Identifier" ).GetValue(oneObject.GetValue(item,null),null);
+                        }
+                        else if(oneObject.PropertyType.IsGenericType && typeof(ICollection<>).IsAssignableFrom(oneObject.PropertyType.GetGenericTypeDefinition()) ||
+                            oneObject.PropertyType.GetInterfaces().Any(x=>x.IsGenericType && x.GetGenericTypeDefinition()== typeof(ICollection<>))){
+                            propertyValue = null;
+                        }
+                        else{
+                            propertyValue = oneObject.GetValue(item, null);
+                        }                    
+                        newLine.Append($"{propertyValue},");
                     }
-                    else if(oneObject.PropertyType.IsGenericType && typeof(ICollection<>).IsAssignableFrom(oneObject.PropertyType.GetGenericTypeDefinition()) ||
-                        oneObject.PropertyType.GetInterfaces().Any(x=>x.IsGenericType && x.GetGenericTypeDefinition()== typeof(ICollection<>))){
-                        propertyValue = null;
-                    }
-                    else{
-                        propertyValue = oneObject.GetValue(item, null);
-                    }                    
-                    newLine.Append($"{propertyValue},");
+                    tmp.AppendLine(newLine.ToString());
                 }
-                tmp.AppendLine(newLine.ToString());
+                File.WriteAllText(path,tmp.ToString());
+                return true;
             }
-            File.WriteAllText(path,tmp.ToString());
+            catch(Exception ex){
+                log.Error(ex);
+                return false; 
+            }
         }
     }
 }
